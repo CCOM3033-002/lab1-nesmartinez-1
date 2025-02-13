40918b593bd9b382468c21846b31b8a6e5ef1d26
nesmartinez-1 <nestor.martinez5@upr.edu>
2025-02-13 00:03:09 +0000

    lab finished

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /* 
-Asignacion 1: Binary Digit
-Nombre: Nestor Martinez
+Lab 1: Box Office
+Nombre: Nestor A. Martinez Rivera
 Num. Est: 802-22-3069
 */
 
@@ -10,11 +10,8 @@ using namespace std;
 
 int main() {
     string name;
-    double adultTickets, childTickets, adultSales, childSales, grossProfit;
-    adultSales = adultTickets * 10.0;
-    childSales = childTickets * 6.0;
-    grossProfit = adultSales + childSales;
-    
+    double adultTickets, childTickets, adultSales, childSales, grossProfit, netProfit, distributorProfit;
+
 
     cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater.\n";
     cout << "\n";
@@ -29,15 +26,21 @@ int main() {
     cin >> childTickets;
 
 
+    adultSales = adultTickets * 10.0;
+    childSales = childTickets * 6.0;
+    grossProfit = adultSales + childSales;
+    netProfit = grossProfit * 0.2;
+    distributorProfit = grossProfit - netProfit; 
+
+
     cout << "\n";
     cout << setprecision(2) << fixed;
-    cout << "Movie name:" << setw(18) << name << endl;
-    cout << "Adult Tickets Sold:" << setw(10) << adultTickets << endl;
-    cout << "Child Tickets Sold:" << setw(10) << childTickets << endl;
-    cout << "Gross Box Office Profit:" << setw(5) << grossProfit << endl;
-    cout << "Net Box Office Profit:" << setw(7) << adultTickets << endl;
-    cout << "Amount Paid to Distributor: " << adultTickets << endl;
-
+    cout << left << setw(30) << "Movie Name:" << "\"" + name + "\"" << endl;
+    cout << left << setw(30) << "Adult Tickets Sold:" << right << setw(10) << static_cast<int>(adultTickets) << endl;
+    cout << left << setw(30) << "Child Tickets Sold:" << right << setw(10) << static_cast<int>(childTickets) << endl;
+    cout << left << setw(30) << "Gross Box Office Profit:" << "$" << setw(9) << right << grossProfit << endl;
+    cout << left << setw(30) << "Net Box Office Profit:" << "$" << setw(9) << right << netProfit << endl;
+    cout << left << setw(30) << "Amount Paid to Distributor:" << "$" << setw(9) << right << distributorProfit << endl;
 
     return 0;
 }
